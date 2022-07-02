b3a5caa025d9d0391cc7143db75c090fba58ed46
Oladipo.Alokan <oladipo.alokan@globalaccelerex.com>
2022-07-02 16:34:32 +0100

    prop-drilling testing

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import FeedbackItem from "./component/FeedbackItem";
 
 function App() {
   const [FeedBackItems, setFeedBackItems] = useState(FeedbackData);
-
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to kill?")) {
+      setFeedBackItems(FeedBackItems.filter((item) => item.id != id));
+    }
+  };
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback = {FeedBackItems} />
+        <FeedbackList feedback={FeedBackItems} handleDelete={deleteFeedback} />
       </div>
     </>
   );

23c1ef6174975e03dc5651a2287516db106a1c9c
Oladipo.Alokan <oladipo.alokan@globalaccelerex.com>
2022-07-02 18:57:12 +0100

    animation fade added

--- src/component/FeedbackList.jsx
+++ src/component/FeedbackList.jsx
@@ -1,21 +1,43 @@
+import { motion, AnimatePresence } from "framer-motion";
 import FeedbackItem from "./FeedbackItem";
 import propTypes from "prop-types";
 
-function FeedbackList({ feedback , handleDelete}) {
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length < 0) {
     return <p>No feedback yet</p>;
   } else {
     return (
       <div className="feedback-list">
-        {feedback.map((item) => (
-          <FeedbackItem
-            key={item.id}
-            item={item}
-            handleDelete={handleDelete}
-          />
-        ))}
+        <AnimatePresence>
+          {feedback.map((item) => (
+            <motion.div
+              key={item.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FeedbackItem
+                key={item.id}
+                item={item}
+                handleDelete={handleDelete}
+              />
+            </motion.div>
+          ))}
+        </AnimatePresence>
       </div>
     );
+
+    // return (
+    //   <div className="feedback-list">
+    //     {feedback.map((item) => (
+    //       <FeedbackItem
+    //         key={item.id}
+    //         item={item}
+    //         handleDelete={handleDelete}
+    //       />
+    //     ))}
+    //   </div>
+    // );
   }
 }
 

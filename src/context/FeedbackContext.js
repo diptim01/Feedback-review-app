450313bf3bcc94fdeb3f872642e39ad8cd03f37f
Oladipo.Alokan <oladipo.alokan@globalaccelerex.com>
2022-07-03 01:54:26 +0100

    Added feedback to mock db and use of proxy in API

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,5 @@
 import { useState, createContext, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
+
 
 const FeedbackContext = createContext();
 
@@ -19,12 +19,10 @@ export const FeedbackProvider = ({ children }) => {
 
   //fetch feedback
   const fetchFeedback = async () => {
-    const response = await fetch(
-      `http://localhost:9000/feedback?_sort=id&_order=desc`
-    );
+    const response = await fetch(`/feedback?_sort=id&_order=desc`);
     const data = await response.json();
     setFeedback(data);
-    setisLoading(false)
+    setisLoading(false);
   };
 
   //update feedbackitem
@@ -51,9 +49,18 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   //add feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body : JSON.stringify(newFeedback)
+    });
+
+   const data = await response.json();
+
+    setFeedback([data, ...feedback]);
   };
 
   return (
